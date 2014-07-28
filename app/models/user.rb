31838eeda60622da4afee7f75d99daabc6b8486b
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-07-27 20:34:47 -0700

    Require bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
 	include BCrypt
 	validates :email, 		presence: true, uniqueness: true
   validates :username, 	presence: true, uniqueness: true
-  validates :password, 	presence: true, langth: { minimum: 6 }
+  validates :password, 	presence: true, length: { minimum: 6 }
 
   def password
   	@password ||= Password.new(password_hash)
