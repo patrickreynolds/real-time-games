1507d603720d3c7f10a47ef58bcc42361bdcd067
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-07-27 22:38:07 -0700

    Fix pusher channel typo

--- app/controllers/game_routes.rb
+++ app/controllers/game_routes.rb
@@ -8,7 +8,7 @@ end
 post '/users/:username/games/jsracer' do
 	puts params
 	if params[:player] == 'player1'
-		Pusher["jsracer-#{ params[:username} }"].trigger('move_player_1', {})
+		Pusher["jsracer-#{ params[:username] }"].trigger('move_player_1', {})
 	elsif params[:player] == 'player2'
 		Pusher["jsracer-#{ params[:username] }"].trigger('move_player_2', {})
 	end
