0954763847ca618b18f4964c0358a7d8681447d1
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-07-27 21:55:10 -0700

    Remove validations temporarily

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ require 'bcrypt'
 
 class User < ActiveRecord::Base
 	include BCrypt
-	validates :email, 		presence: true, uniqueness: true
-  validates :username, 	presence: true, uniqueness: true
-  validates :password, 	presence: true, length: { minimum: 6 }
+	# validates :email, 		presence: true, uniqueness: true
+ #  validates :username, 	presence: true, uniqueness: true
+ #  validates :password, 	presence: true, length: { minimum: 6 }
 
   def password
   	@password ||= Password.new(password_hash)
