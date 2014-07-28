ef897d918182d4d782924e4da325acd47bfbd20d
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-07-27 20:20:17 -0700

    Add bcrypt for user authentication/authorization.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
+	include BCrypt
 	validates :email, 		presence: true, uniqueness: true
   validates :username, 	presence: true, uniqueness: true
   validates :password, 	presence: true, langth: { minimum: 6 }
+
+  def password
+  	@password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+  	@password = Password.create(new_password)
+  	self.password_hash = @password
+  end
 end
--- db/migrate/20140727193605_create_users.rb
+++ db/migrate/20140727193605_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
   	create_table :users do |t|
   		t.string :email
   		t.string :username
-  		t.string :password
+  		t.string :password_hash
 
   		t.timestamps
   	end
